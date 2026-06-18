a1f747b3ff84ad47c1373da29b4e77b075911107
jesus tornero <jesus_tornero@outlook.com>
2026-06-18 09:56:34 -0500

    Infraestructura i18n

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,31 +1,51 @@
 import i18n from 'i18next';
+
 import { initReactI18next } from 'react-i18next';
 
-import enCommon from '@translations/en/common.json';
-import esCommon from '@translations/es/common.json';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
-const STORAGE_KEY = 'portfolio-language';
+import esCommon from '../translations/es/common.json';
+import esNavigation from '../translations/es/navigation.json';
+import esHomepage from '../translations/es/homepage.json';
 
-const savedLanguage =
-  localStorage.getItem(STORAGE_KEY) ?? 'es';
+import enCommon from '../translations/en/common.json';
+import enNavigation from '../translations/en/navigation.json';
+import enHomepage from '../translations/en/homepage.json';
 
-void i18n.use(initReactI18next).init({
-  lng: savedLanguage,
-  fallbackLng: 'en',
+void i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'es',
 
-  interpolation: {
-    escapeValue: false,
-  },
+    lng: 'es',
 
-  resources: {
-    es: {
-      translation: esCommon,
+    supportedLngs: [
+      'es',
+      'en',
+    ],
+
+    interpolation: {
+      escapeValue: false,
     },
 
-    en: {
-      translation: enCommon,
+    resources: {
+      es: {
+        common: esCommon,
+        navigation:
+          esNavigation,
+        homepage:
+          esHomepage,
+      },
+
+      en: {
+        common: enCommon,
+        navigation:
+          enNavigation,
+        homepage:
+          enHomepage,
+      },
     },
-  },
-});
+  });
 
 export default i18n;
