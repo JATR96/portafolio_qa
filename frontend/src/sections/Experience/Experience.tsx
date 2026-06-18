01482a94a1cf31c4492d5ddb902c171c69997b01
jesus tornero <jesus_tornero@outlook.com>
2026-06-18 18:59:22 -0500

    add experience v2

--- frontend/src/sections/Experience/Experience.tsx
+++ frontend/src/sections/Experience/Experience.tsx
@@ -6,21 +6,187 @@ import styles from './Experience.module.scss';
 
 const Experience =
   (): React.JSX.Element => {
+    const { t } =
+      useTranslation(
+        'experience',
+      );
+
     return (
-      <section id="experience">
-        {experiences.map(
-          (item) => (
-            <article key={item.id}>
-              <h3>{item.role}</h3>
+      <section
+        id="experience"
+        className={styles.experience}
+      >
+        <div
+          className={styles.container}
+        >
+          <h2
+            className={styles.title}
+          >
+            {t(
+              'section.title',
+            )}
+          </h2>
 
-              <p>{item.company}</p>
+          <p
+            className={
+              styles.description
+            }
+          >
+            {t(
+              'section.description',
+            )}
+          </p>
 
-              <span>
-                {item.period}
-              </span>
-            </article>
-          ),
-        )}
+          <div
+            className={
+              styles.timeline
+            }
+          >
+            {experiences.map(
+              (
+                experience,
+              ) => (
+                <article
+                  key={
+                    experience.id
+                  }
+                  className={
+                    styles.companyCard
+                  }
+                >
+                  <header
+                    className={
+                      styles.companyHeader
+                    }
+                  >
+                    <h3>
+                      {
+                        experience.role
+                      }
+                    </h3>
+
+                    <h4>
+                      {
+                        experience.company
+                      }
+                    </h4>
+
+                    <span>
+                      {
+                        experience.period
+                      }
+                    </span>
+                  </header>
+
+                  <div
+                    className={
+                      styles.projects
+                    }
+                  >
+                    {experience.projects.map(
+                      (
+                        project,
+                      ) => {
+                        const responsibilities =
+                          t(
+                            `companies.${experience.id}.projects.${project.id}.responsibilities`,
+                            {
+                              returnObjects:
+                                true,
+                            },
+                          ) as string[];
+
+                        return (
+                          <div
+                            key={
+                              project.id
+                            }
+                            className={
+                              styles.projectCard
+                            }
+                          >
+                            <h5>
+                              {t(
+                                `companies.${experience.id}.projects.${project.id}.name`,
+                              )}
+                            </h5>
+
+                            <p
+                              className={
+                                styles.duration
+                              }
+                            >
+                              {
+                                project.duration
+                              }
+                            </p>
+
+                            <p
+                              className={
+                                styles.projectDescription
+                              }
+                            >
+                              {t(
+                                `companies.${experience.id}.projects.${project.id}.description`,
+                              )}
+                            </p>
+
+                            <ul
+                              className={
+                                styles.responsibilities
+                              }
+                            >
+                              {responsibilities.map(
+                                (
+                                  responsibility,
+                                ) => (
+                                  <li
+                                    key={
+                                      responsibility
+                                    }
+                                  >
+                                    {
+                                      responsibility
+                                    }
+                                  </li>
+                                ),
+                              )}
+                            </ul>
+
+                            <div
+                              className={
+                                styles.badges
+                              }
+                            >
+                              {project.technologies.map(
+                                (
+                                  technology,
+                                ) => (
+                                  <span
+                                    key={
+                                      technology
+                                    }
+                                    className={
+                                      styles.badge
+                                    }
+                                  >
+                                    {
+                                      technology
+                                    }
+                                  </span>
+                                ),
+                              )}
+                            </div>
+                          </div>
+                        );
+                      },
+                    )}
+                  </div>
+                </article>
+              ),
+            )}
+          </div>
+        </div>
       </section>
     );
   };
