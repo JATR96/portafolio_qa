ca41940a20d30bfd87890f26675d8af3fc7651ec
jesus tornero <jesus_tornero@outlook.com>
2026-06-19 19:30:26 -0500

    Scroll To Top Enterprise

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
-import AppRouter from '@routes/index';
+import AppRouter
+  from '@routes/index';
 
-const App = (): React.JSX.Element => {
-  return <AppRouter />;
-};
+import ScrollToTop
+  from '@components/ScrollToTop/ScrollToTop';
+
+const App =
+  (): React.JSX.Element => {
+    return (
+      <>
+        <ScrollToTop />
+
+        <AppRouter />
+      </>
+    );
+  };
 
 export default App;
